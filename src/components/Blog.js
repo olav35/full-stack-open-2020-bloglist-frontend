71646721bd47422382c0fa4cfd8b679e81976478
Olav Fosse <fosseolav@gmail.com>
2020-07-14 18:19:04 +0200

    Add full view

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,27 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [fullView, setFullView] = useState(false)
+
+  const toggleFullView = () => setFullView(!fullView)
+
+  return (
+    <div>
+      {fullView && <br/>}
+      {blog.title} {blog.author} <button onClick={toggleFullView}>{fullView ? 'hide' : 'view'}</button>
+      { 
+        fullView && (
+          <div>
+            {blog.url}
+            <br/>
+            likes {blog.likes} <button>like</button> {/* dummy button*/}
+            <br/>
+            {blog.user.name}
+          </div>
+        )
+      }
+      {fullView && <br/>}
+    </div>
+  )
+}
 
 export default Blog
