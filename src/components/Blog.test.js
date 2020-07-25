14194453587b794aa6d748a03dec2ee012c7de9d
Olav Fosse <fosseolav@gmail.com>
2020-07-25 22:23:57 +0200

    Test Blog component
    Test that blog likes and the url is displayed when the view button is
    clicked.

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders the blog\'s title and author', () => {
@@ -19,4 +19,28 @@ test('renders the blog\'s title and author', () => {
   expect(component.container).toHaveTextContent(blog.author)
   expect(component.container).not.toHaveTextContent(blog.likes)
   expect(component.container).not.toHaveTextContent(blog.url)
+})
+
+test('renders the url and likes when view is clicked', () => {
+  const user = {
+    name: 'Olav',
+    username: 'olavfosse'
+  }
+  const blog = {
+    title: 'Blog Title',
+    author: 'Blog Author',
+    likes: 100,
+    url: 'https://sickurl.com',
+    user
+  }
+
+  const component = render(
+    <Blog blog={blog} user={user} onClickLike={() => undefined} onClickRemoveBlog={() => undefined}s/>
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent(blog.likes)
+  expect(component.container).toHaveTextContent(blog.url)
 })
