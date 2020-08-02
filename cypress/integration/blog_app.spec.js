7e4b949545675ebb72a67de413e618e90ce38722
Olav Fosse <fosseolav@gmail.com>
2020-08-02 21:49:03 +0200

    Test successful and unsuccessful login

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,13 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    // Create user
+    const user = {
+      name: 'user',
+      username: 'username',
+      password: 'password'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +16,21 @@ describe('Blog app', function() {
     cy.contains('log in to application')
     cy.contains('username:')
     cy.contains('password:')
+  })
+
+  describe('Login', function () {
+    it('suceeds with correct credentials', function () {
+      cy.get('#username-input').type('username')
+      cy.get('#password-input').type('password')
+      cy.get('#login-form').submit()
+      cy.contains('user logged in')
+    })
+
+    it('fails with incorrecet credentials', function () {
+      cy.get('#username-input').type('wrongusername')
+      cy.get('#password-input').type('wrongpassword')
+      cy.get('#login-form').submit()
+      cy.contains('user logged in').should('not.exist')
+    })
   })
 })
--- src/components/Login.js
+++ src/components/Login.js
@@ -10,11 +10,11 @@ const Login = (props) => {
   } = props
 
   return (
-    <form onSubmit={onLogin}>
+    <form id="login-form" onSubmit={onLogin}>
       <h2>log in to application</h2>
-      <b>username:</b><input onChange={onUsernameChange} value={username}/>
+      <b>username:</b><input id="username-input" onChange={onUsernameChange} value={username}/>
       <br/>
-      <b>password:</b><input type="password" onChange={onPasswordChange} value={password}/>
+      <b>password:</b><input id="password-input" type="password" onChange={onPasswordChange} value={password}/>
       <br/>
       <input type="submit" value="Login"/>
     </form>
